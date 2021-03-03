0926b07c591d9addc9a3909afc6aa905d17a26bd
Bayuardana <79974655+Bayuardana@users.noreply.github.com>
2021-03-03 18:39:31 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'Admin DrawlNag', m)
+  // this.sendContact(m.chat, '6281908786197', 'Rara', m)
+  this.sendContact(m.chat, '6287780597451', 'BayuArdana', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
